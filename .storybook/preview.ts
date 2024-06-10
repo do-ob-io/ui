2f4057299cbf761939074e840dc585528424b5aa
Eric Crowell <eric.crowell@ecrow.dev>
2024-06-10 02:30:59 +0200

    feat: Added theming and a11y to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import { withThemeByClassName } from '@storybook/addon-themes';
 import '../global.css';
 
 const preview: Preview = {
@@ -11,5 +12,15 @@ const preview: Preview = {
     },
   },
 };
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+];
 
 export default preview;
--- packages/ui/src/Navigation/Navigation.tsx
+++ packages/ui/src/Navigation/Navigation.tsx
@@ -1,13 +1,13 @@
 import { Navbar, NavbarContent, NavbarBrand } from '@nextui-org/react';
 
 export function Navigation() {
-    return (
-        <Navbar className=''>
-            <NavbarContent>
-                <NavbarBrand>NextUI</NavbarBrand>
-            </NavbarContent>
-        </Navbar>
-    );
+  return (
+    <Navbar className="">
+      <NavbarContent>
+        <NavbarBrand>NextUI</NavbarBrand>
+      </NavbarContent>
+    </Navbar>
+  );
 }
 
 export default Navigation;
