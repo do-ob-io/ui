6a95a880ec23af995fef6011886f5930d4d5474b
Eric Crowell <eric.crowell@ecrow.dev>
2024-06-15 00:12:33 +0200

    feat: Added react compiler eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,16 @@
 import doob from '@do-ob/eslint-config';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default [
   ...doob.configs.recommended,
+
+  {
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      ...reactCompiler.rules.recommended,
+      'react-compiler/react-compiler': 'error',
+    },
+  },
 ];
