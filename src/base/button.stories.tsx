a96a1dbae6fc100dbbe63ed4d64c8fa9a7927d13
eCrow <eric.crowell@ecrow.dev>
2025-12-28 11:49:51 +0000

    test: add icon button story

--- src/base/button.stories.tsx
+++ src/base/button.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import { ArrowUpIcon } from 'lucide-react';
 
 import { Button } from './button';
 
@@ -9,11 +10,20 @@ const meta = {
   },
   tags: [ 'autodocs' ],
   args: {
-    children: 'Click Here'
-  }
+    children: 'Click Here',
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {};
+
+export const Icon: Story = {
+  args: {
+    size: 'icon',
+    children: (
+      <ArrowUpIcon />
+    ),
+  },
+};
