aa176c2f23bba1f3a220cdebf3fa7a851c316038
eCrow <eric.crowell@ecrow.dev>
2025-12-29 07:22:05 +0000

    build: add build typescript configuration

--- src/base/button.tsx
+++ src/base/button.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/utils';
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   'focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 rounded-md border border-transparent bg-clip-padding text-sm font-medium focus-visible:ring-[3px] aria-invalid:ring-[3px] [&_svg:not([class*=\'size-\'])]:size-4 inline-flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none group/button select-none',
   {
     variants: {
@@ -33,12 +33,14 @@ const buttonVariants = cva(
   },
 );
 
-function Button({
+export type ButtonProps = ButtonPrimitive.Props & VariantProps<typeof buttonVariants>;
+
+export function Button({
   className,
   variant = 'default',
   size = 'default',
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: ButtonProps) {
   return (
     <ButtonPrimitive
       data-slot="button"
@@ -47,5 +49,3 @@ function Button({
     />
   );
 }
-
-export { Button, buttonVariants };
