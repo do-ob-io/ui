d4ca08b82288fb35179fdf49eeb019a14e6aa335
eric.crowell <eric.crowell@ecrow.dev>
2026-03-15 12:35:13 +0000

    ci: specify tsconfig for library build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
+  tsconfig: 'tsconfig.lib.json',
   entry: [ 'src/index.ts', 'src/base/index.ts' ],
   exports: true,
   minify: true,
